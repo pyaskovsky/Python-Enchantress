2d7f70a789d4b4871db5fe4e60d96f243d5dc74f
Ihor Piaskovskyi <ihor.piaskovskyi@forbytes.com>
2021-01-10 20:49:18 +0200

    Added apply discount

--- homework/Person.py
+++ homework/Person.py
@@ -8,13 +8,23 @@ HOUSES = [
 ]
 
 
+class House:
+    def __init__(self, area: int, cost: int):
+        self.area = area
+        self.cost = cost
+
+    def show_home_info(self):
+        print(f'Home with area: {self.area}m2 and cost: ${self.cost}')
+
+
+
 class Human(ABC):
     @abstractmethod
     def info(self):
         raise NotImplemented('No data to display.')
 
     @abstractmethod
-    def earn_money(self):
+    def earn_money(self, money: int):
         raise NotImplemented("I haven't money.")
 
     @abstractmethod
@@ -27,21 +37,28 @@ class Person(Human):
         self.name = name
         self.age = age
         self.availability_money = 0
-        self.has_home = []
+        self.has_home = False
 
     def info(self):
-        has_own_home = 'Yes' if len(self.has_home) else 'No'
-
         print(f'''
 Person information:
 Name: {self.name}
 Age: {self.age}
 Availability of money: ${self.availability_money}
-Having your own home: {has_own_home}
+Having your own home: {self.has_home}
 ''')
 
     def earn_money(self, money):
         self.availability_money += money
+
+    @staticmethod
+    def apply_discount(house: House) -> None:
+        small_home = 40
+        price = house.cost
+        discount = 15 if house.area > small_home else 0
+        if discount > 0:
+            price = int(price - (discount / 100) * price)
+        print(f'Home with area: {house.area}m2 and cost: ${price} and your discount is {discount}%')
 
     def buy_house(self):
         print('Choose home:')
@@ -53,37 +70,17 @@ Having your own home: {has_own_home}
 
         home_position = int(input("Home: "))
 
-        home_area = HOUSES[home_position - 1]["area"]
-        home_cost = HOUSES[home_position - 1]["cost"]
+        house = HOUSES[home_position - 1]
+        home_area = house["area"]
+        home_cost = house["cost"]
 
         if self.availability_money >= home_cost:
+            self.has_home = True
             print("You bought:")
-            self.has_home.append(House(home_area, home_cost))
+
+            self.apply_discount(House(home_area, home_cost))
         else:
             print("Please earn more money")
-
-    def show_available_houses(self):
-        for home in self.has_home:
-            area = home.area
-            cost = home.cost
-
-            home = House(area, cost)
-            home.show_home_info()
-
-
-class House:
-    def __init__(self, area, cost):
-        self.area = area
-        self.cost = cost
-
-    def show_home_info(self):
-        small_home = 40
-        price = self.cost
-        discount = 15 if self.area > small_home else 0
-        if discount > 0:
-            price = int(price - (discount / 100) * price)
-
-        print(f'Home with area: {self.area}m2 and cost: ${price} and your discount is {discount}%')
 
 
 if __name__ == '__main__':
@@ -91,10 +88,8 @@ if __name__ == '__main__':
     human1.earn_money(50000)
     human1.info()
     human1.buy_house()
-    human1.show_available_houses()
 
     human2 = Person("Ivan", 30)
     human2.earn_money(600000)
     human2.info()
     human2.buy_house()
-    human2.show_available_houses()
