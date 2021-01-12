b4951743d280eaf9987c60cc59d576d3b3f42277
Ihor Piaskovskyi <ihor.piaskovskyi@forbytes.com>
2021-01-12 18:15:28 +0200

    Refactored

--- homework/tests/test_simple_calc.py
+++ homework/tests/test_simple_calc.py
@@ -1,22 +1,19 @@
 import unittest
-from homework.lib.simple_calc import Calculator
+from homework.test_simple_calc import add, subtract, multiply, divide
 
 
 class TestCalc(unittest.TestCase):
-    def setUp(self):
-        self.calc = Calculator()
-
     def test_add(self):
-        self.assertEqual(self.calc.add(2, 5), 7)
+        self.assertEqual(add(2, 5), 7)
 
     def test_subtract(self):
-        self.assertEqual(self.calc.subtract(8, 2), 6)
+        self.assertEqual(subtract(8, 2), 6)
 
     def test_multiply(self):
-        self.assertEqual(self.calc.multiply(2, 6), 12)
+        self.assertEqual(multiply(2, 6), 12)
 
     def test_divide(self):
-        self.assertEqual(self.calc.divide(20, 10), 2)
+        self.assertEqual(divide(20, 10), 2)
 
 
 if __name__ == '__main__':
