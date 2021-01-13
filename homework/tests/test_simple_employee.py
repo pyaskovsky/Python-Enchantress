b9aa8d83fcc28d2d78428691c53e83bf2adf2b97
Ihor Piaskovskyi <ihor.piaskovskyi@forbytes.com>
2021-01-13 20:06:24 +0200

    Add test for apply_raise

--- homework/tests/test_simple_employee.py
+++ homework/tests/test_simple_employee.py
@@ -6,7 +6,7 @@ from unittest import mock
 
 class TestEmployee(unittest.TestCase):
     def setUp(self):
-        self.employee = Employee('Ihor', 'Piaskovskyi', 100)
+        self.employee = Employee('Ihor', 'Piaskovskyi', 56000)
 
     def test_email(self):
         self.assertEqual(self.employee.email, 'Ihor.Piaskovskyi@email.com')
@@ -15,7 +15,8 @@ class TestEmployee(unittest.TestCase):
         self.assertEqual(self.employee.fullname, 'Ihor Piaskovskyi')
 
     def test_apply_raise(self):
-        pass
+        self.employee.apply_raise()
+        self.assertEqual(self.employee.pay, 58800)
 
     @mock.patch("homework.tests.utils.make_request", return_value=True)
     def test_monthly_schedule(self, month):
